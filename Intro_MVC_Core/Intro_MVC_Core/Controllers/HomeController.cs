f20d0615d2590ca522618438b76976f2d8851650
thejamilah <jamilahmcw@gmail.com>
2018-10-23 13:59:27 -0700

    MVC Shell Created

---
+++ Intro_MVC_Core/Intro_MVC_Core/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Intro_MVC_Core.Controllers
+{
+    public class HomeController : Controller
+    {
+        public void Index()
+        {
+
+        }
+
+        public ViewResult Class()
+        {
+            return View();
+        }
+    }
+}
